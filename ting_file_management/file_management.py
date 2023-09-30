f8c5497b6964deb3c18d096c4d0422ece801efe9
Jefferson Santos <jeff-ds@outlook.com>
2023-09-29 22:04:09 -0300

    requirement 02 - create txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+    if not path_file.endswith('.txt'):
+        return sys.stderr.write("Formato inválido\n")
+
+    with open(path_file, 'r', encoding='utf-8') as file:
+        return file.read().split('\n')
