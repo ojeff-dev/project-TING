cf4142b4ad9b7ff28588ad2240bbbc3fc1e16b1f
Jefferson Santos <jeff-ds@outlook.com>
2023-09-29 21:19:46 -0300

    requirement 01 - create Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,22 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self.__len__() > 0:
+            return self._data.pop(0)
+        else:
+            return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if not (0 <= index <= self.__len__() - 1):
+            raise IndexError("Índice Inválido ou Inexistente")
+
+        return self._data[index]
