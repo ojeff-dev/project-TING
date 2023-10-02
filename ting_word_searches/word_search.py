8189aaf1a4256b83acd9e8d4840d6b4819065691
Jefferson Santos <jeff-ds@outlook.com>
2023-10-02 16:24:13 -0300

    requirement 07 - implements exists_word function

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    lower_word = word.lower()
+    data = []
+
+    for i in range(instance.__len__()):
+        file = instance.search(i)
+
+        words = file["linhas_do_arquivo"]
+        occurrences = []
+
+        for index, w in enumerate(words):
+            if lower_word in w.lower():
+                occurrences.append({"linha": index + 1})
+
+        if occurrences:
+            data.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return data
 
 
 def search_by_word(word, instance):
