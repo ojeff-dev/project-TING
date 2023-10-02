8337bf36a7ab90f0fac939f938c7f67542db2cb6
Jefferson Santos <jeff-ds@outlook.com>
2023-10-02 13:10:42 -0300

    requirement 03 - implements process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+
+    if not isinstance(file, list):
+        return None
+
+    dict_file = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+
+    if instance.__len__() > 0:
+        for i in range(instance.__len__()):
+            if instance.search(i)["nome_do_arquivo"] == path_file:
+                return None
+
+    instance.enqueue(dict_file)
+    sys.stdout.write(str(dict_file))
 
 
 def remove(instance):
