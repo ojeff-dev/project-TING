3c8d51edac44029946cffbda3f7760e49a04f746
Jefferson Santos <jeff-ds@outlook.com>
2023-10-02 15:05:53 -0300

    requirement 06 - implements PriorityQueue tests

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    queue = PriorityQueue()
+
+    for i in range(1, 5):
+        queue.enqueue({"qtd_linhas": i * 2})
+
+    assert queue.__len__() == 4
+    assert queue.high_priority.__len__() == 2
+    assert queue.regular_priority.__len__() == 2
+
+    queue.dequeue()
+    assert queue.__len__() == 3
+    assert queue.high_priority.__len__() == 1
+    assert queue.regular_priority.__len__() == 2
+
+    file = queue.search(0)
+    assert file["qtd_linhas"] == 4
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        queue.search(99999)
